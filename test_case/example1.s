ec3987cc0955abaea7fbe9def875683ccb347521
Ze Zhang <zezhang@umich.edu>
2015-11-14 17:40:07 -0500

    upload test case

---
+++ test_case/example1.s
@@ -0,0 +1,43 @@
+	.file	"main.c"
+	.text
+	.globl	function
+	.type	function, @function
+function:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -36(%rbp)
+	movl	%esi, -40(%rbp)
+	movl	%edx, -44(%rbp)
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	function, .-function
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$3, %edx
+	movl	$2, %esi
+	movl	$1, %edi
+	call	function
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.3 20140911 (Red Hat 4.8.3-9)"
+	.section	.note.GNU-stack,"",@progbits
